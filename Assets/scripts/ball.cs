44ec9ac7429e8b9d21b0e9b7f61aaf00057e3711
diyon <Diyonromijn2007@gmail.com>
2023-10-03 15:25:02 +0200

    final product

---
+++ Assets/scripts/ball.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ball : MonoBehaviour
+{
+    public float xPosition = -2f;
+    public float yPosition = -2f;
+    public float xSpeed = 1f;
+    public float ySpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        xPosition = xPosition + xSpeed * Time.deltaTime; 
+        yPosition = yPosition + ySpeed * Time.deltaTime;
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+
+
+
+
+    }
+}
